f0ad01e1c05a39cd74c484930855824112caf7a3
riaz9191 <riazahammed9191@gmail.com>
2023-05-18 20:49:28 +0600

    post done

--- index.js
+++ index.js
@@ -16,24 +16,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const toyCollections = client.db("toyMarket").collection('toys')
+
+        //postToys
+        app.post('/posttoys', async (req, res) => {
+            const body = req.body;
+            if (!body) {
+                return res.status(404).send({ message: "Invalid request." });
+            }
+            console.log(body)
+            const result = await toyCollections.insertOne(body)
+            res.send(result)
+        })
+        app.get('/allToys', async (req, res) => {
+            const result = await toyCollections.find({}).toArray();
+            res.send(result)
+            console.log(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
